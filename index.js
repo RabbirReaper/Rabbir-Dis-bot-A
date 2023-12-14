a926d2d6bad297bca77f416ccb58069e46948cb9
Rabbir_Reaper <abc0979999412@gmail.com>
2023-12-15 01:31:05 +0800

    檔案初始化

---
+++ index.js
@@ -0,0 +1,11 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.Token);
